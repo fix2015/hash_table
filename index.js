d33db0ad8f59a1e216f0824356cbead586329ad3
Semianchuk Vitalii <fix20152@gmail.com>
2025-01-18 13:54:11 +0000

    Create task file

---
+++ index.js
@@ -0,0 +1,30 @@
+class HashTable {
+    constructor(size = 50) {
+
+ }
+
+  // Hash function to calculate the index
+  hash(key) {
+     
+  }
+
+  // Insert a key-value pair into the hash table
+  insert(key, value) {
+    
+  }
+
+  // Delete a key-value pair from the hash table
+  delete(key) {
+      
+  }
+
+  // Search for a value by its key
+  search(key) {
+     
+  }
+
+  // Get the size of the hash table
+  size() {
+    
+  }
+}
